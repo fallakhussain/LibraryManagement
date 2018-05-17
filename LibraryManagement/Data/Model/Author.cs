ee881516bfa77eb3328a92a158785e373f2c3c3d
Fallak <fallakhussain@hotmail.co.uk>
2018-05-18 00:54:04 +0100

    Implementing The Data Structure

---
+++ LibraryManagement/Data/Model/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Model
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string Name { get; set; }
+
+        //Relationship to Book Class
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
