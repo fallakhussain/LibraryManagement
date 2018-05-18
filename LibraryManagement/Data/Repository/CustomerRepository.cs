9b735c81c7089794ceca8ce652f93e4268a8ad9c
Fallak <fallakhussain@hotmail.co.uk>
2018-05-18 02:00:56 +0100

    Customer Repository creation

---
+++ LibraryManagement/Data/Repository/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagement.Data.Interfaces;
+using LibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Repository
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(LibraryDbContext contex) : base(contex)
+        {
+
+        }
+    }
+}
