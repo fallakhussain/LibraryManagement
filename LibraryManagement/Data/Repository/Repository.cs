74e0da74e9d90597f1cce5c1eb37f42daf6ac587
Fallak <fallakhussain@hotmail.co.uk>
2018-05-18 01:29:34 +0100

    Repository Generic Functions Class implementation

---
+++ LibraryManagement/Data/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using LibraryManagement.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly LibraryDbContext _context;
+
+        public int Count(Func<T, bool> precidate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
