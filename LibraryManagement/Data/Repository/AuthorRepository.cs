9c22b21ecf96ca8aad080561ab10ad90b5843e1c
Fallak <fallakhussain@hotmail.co.uk>
2018-05-18 01:52:38 +0100

    Create Author Repository implentation with two interfaces

---
+++ LibraryManagement/Data/Repository/AuthorRepository.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManagement.Data.Interfaces;
+using LibraryManagement.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Repository
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(LibraryDbContext contex) : base(contex)
+        {
+
+        }
+
+        public IEnumerable<Author> GetAllWithBooks() => _context.Authors.Include(a => a.Books);
+
+        public Author GetWithBooks(int id)
+        {
+            return _context.Authors
+                .Where(a => a.AuthorID == id)
+                .Include(a => a.Books)
+                .FirstOrDefault();
+        }
+    }
+}
