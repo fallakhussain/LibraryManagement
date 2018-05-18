f91f0c6f266dd0a7f2279f541e99247912a8d592
Fallak <fallakhussain@hotmail.co.uk>
2018-05-18 01:15:28 +0100

    Repository Interfaces - Defining functions

---
+++ LibraryManagement/Data/Interfaces/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Interfaces
+{
+    public interface IBookRepository
+    {
+        //Get all books with author
+        IEnumerable<Book> GetAllWithAuthor();
+        //Find a book with author
+        IEnumerable<Book> FindWithAuthor(Func<Book, bool> predicate);
+        //Find a book with author and borrower
+        IEnumerable<Book> FindWithAuthorAndBorrower(Func<Book, bool> predicate);
+    }
+}
---
+++ LibraryManagement/Data/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        //Filter and find
+        IEnumerable<T> Find(Func<T, bool> predicate);
+
+        T GetById(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int Count(Func<T, bool> precidate);
+
+    }
+}
