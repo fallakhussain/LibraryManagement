a01cb7d4c9647ebb19c11e69d854157975602b25
Fallak <fallakhussain@hotmail.co.uk>
2018-05-20 01:33:01 +0100

    Data Model Validation

--- LibraryManagement/Data/Model/Book.cs
+++ LibraryManagement/Data/Model/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace LibraryManagement.Data.Model
     public class Book
     {
         public int BookID { get; set; }
+        [Required, MinLength(3), MaxLength(60)]
         public string Title { get; set; }
 
         public virtual Author Author { get; set; }
